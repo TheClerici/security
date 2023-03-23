f546c3575cac02bdf7b0de6953d8e6517ecc9bca
TheClerici <enrique_cle97@hotmail.com>
2023-03-23 17:49:38 -0600

    first jwt security commit

---
+++ src/main/java/com/cleri/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.cleri.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/cleri/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleri.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
